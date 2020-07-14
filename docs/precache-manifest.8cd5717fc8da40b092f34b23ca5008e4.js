ed744c7c9c40463542d4f52dea2ca55dd9f207f1
FLANAX550 <jafb321@gmail.com>
2020-07-13 18:44:27 -0700

    agregado DOCS

---
+++ docs/precache-manifest.8cd5717fc8da40b092f34b23ca5008e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e108bf4679ec23709a9fdab6370edca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f79b912c501c7dc2e333",
+    "url": "/static/css/main.0aa44c94.chunk.css"
+  },
+  {
+    "revision": "add8aa96ddeda23a07f9",
+    "url": "/static/js/2.210237bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.210237bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79b912c501c7dc2e333",
+    "url": "/static/js/main.0dd1c8fe.chunk.js"
+  },
+  {
+    "revision": "96adf8467d5336713967",
+    "url": "/static/js/runtime-main.beb02c95.js"
+  }
+]);
